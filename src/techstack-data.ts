ba97bb71cd10cca07d86fc07070f0c37d1390bc5
elizielx <yehezkieldio35@gmail.com>
2023-12-09 08:40:04 +0800

    feat!: stuff

---
+++ src/techstack-data.ts
@@ -0,0 +1,86 @@
+export const techData: { name: string; icon: string }[] = [
+    {
+        name: "TypeScript",
+        icon: "typescript",
+    },
+    {
+        name: "PHP",
+        icon: "php",
+    },
+    {
+        name: "Sass",
+        icon: "sass",
+    },
+    {
+        name: "TailwindCSS",
+        icon: "tailwindcss",
+    },
+    {
+        name: "Bulma",
+        icon: "bulma",
+    },
+    {
+        name: "Laravel",
+        icon: "laravel",
+    },
+    {
+        name: "Node.js",
+        icon: "node.js",
+    },
+    {
+        name: "Bun",
+        icon: "bun",
+    },
+    {
+        name: "Qwik",
+        icon: "qwik",
+    },
+    {
+        name: "React",
+        icon: "react",
+    },
+    {
+        name: "Vite",
+        icon: "vite",
+    },
+    {
+        name: "Next.js",
+        icon: "next.js",
+    },
+    {
+        name: "Fastify",
+        icon: "fastify",
+    },
+    {
+        name: "Nest.js",
+        icon: "nestjs",
+    },
+    {
+        name: "Astro",
+        icon: "astro",
+    },
+    {
+        name: "Nx",
+        icon: "nx",
+    },
+    {
+        name: "Docker",
+        icon: "docker",
+    },
+    {
+        name: "tRPC",
+        icon: "trpc",
+    },
+    {
+        name: "Drizzle",
+        icon: "drizzle",
+    },
+    {
+        name: "Prisma",
+        icon: "prisma",
+    },
+    {
+        name: "PostgreSQL",
+        icon: "postgresql",
+    },
+];
